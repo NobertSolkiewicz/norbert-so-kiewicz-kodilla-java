c34a87b03d0832ec274ff63c18e62ef0a55ac59d
Norbert <norsol2200@gmail.com>
2023-03-26 18:38:59 +0200

    7.8 Rock, Paper, Scissors

--- kodilla-rps/src/main/java/com/kodilla/rps/Game.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Game.java
@@ -1,37 +1,81 @@
 package com.kodilla.rps;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     //całę odczytywanie z klawiatury zrobić scannerem i
     // wszyzstko jedynm ciurkiem a potem to wszystko rozbijać
-    private final Rock rock = new Rock(1, "Rock");
-    private final Paper paper = new Paper(2, "Paper");
-    private final Scissors scissors = new Scissors(3, "Scissors");
 
+    Scanner scanner = new Scanner(System.in);
     public void run() {
-        Scanner scanner = new Scanner(System.in);
+        int roundscount = 0;
+        int yourScore = 0;
+        int computerScore = 0;
         boolean end = false;
-
+        System.out.println("Please enter Your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter number of rounds: ");
+        int roundsNumbers = scanner.nextInt();
+        System.out.println("Infomation about game: ");
+        System.out.println("1 - rock");
+        System.out.println("2 - paper");
+        System.out.println("3 - scissors");
+        System.out.println("x - end of the game");
+        System.out.println("n - restart the game");
         while (!end) {
-            String readName = scanner.nextLine();
+            roundscount++;
+            String move = scanner.nextLine();
+            Random random = new Random();
+            int computerMove = random.nextInt(1, 3);
+            System.out.println("Please choose your move");
+            if (move.equals("1") && computerMove == 1) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("1") && computerMove == 2) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                computerScore++;
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("1") && computerMove == 3) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                yourScore++;
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("2") && computerMove == 1) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                yourScore++;
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("2") && computerMove == 2) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("2") && computerMove == 3) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                computerScore++;
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("3") && computerMove == 1) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                computerScore++;
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("3") && computerMove == 2) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                yourScore++;
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("3")&& computerMove == 3) {
+                System.out.println(name + "'s move is: " + move + " Computer move is: " + computerMove);
+                System.out.println("Result is: "+ yourScore + ":" + computerScore + ", the number of rounds is: " + roundscount);
+            } else if (move.equals("x")) {
+                end = true;
+            } else if (move.equals("n")) {
+                Game game = new Game();
+                game.run();
+            }
 
-            Game game = new Game();
-            if (readName.equals("Rock")) {
-
+            if (yourScore >= roundsNumbers || computerScore >= roundsNumbers) {
+                end = true;
             }
         }
-    }
-
-    public Rock getRock() {
-        return rock;
-    }
-
-    public Paper getPaper() {
-        return paper;
-    }
-
-    public Scissors getScissors() {
-        return scissors;
+        if (yourScore > computerScore) {
+            System.out.println("The winner is: " + name);
+        } else
+            System.out.println("The winner is: Computer");
     }
 }
--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -4,17 +4,7 @@ import java.util.Scanner;
 
 public class RpsRunner {
     public static void main(String[] args) {
-        System.out.println("Please enter your name: ");
-        Scanner scanner = new Scanner(System.in);
-        String readName = scanner.nextLine();
-        int readRounds = scanner.nextInt();
-        System.out.println("Enter number of winning round to finish game: ");
-        boolean end = false;
-
-        while (!end) {
-            Game game = new Game();
-
-        }
-
+        Game game = new Game();
+        game.run();
     }
 }
