e7c1e944f9a17a62af54f4cb1e02fc7c4ef1c109
Norbert <norsol2200@gmail.com>
2023-05-20 18:07:13 +0200

    116.3 CREATING TABLE WITH TASKS

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -10,11 +10,14 @@ public class SimpleInvoiceTestSuite {
     void testGetValueToPay() {
         //Given
         SimpleInvoice invoice = new SimpleInvoice();
+        SimpleProduct simpleProduct1 = new SimpleProduct("Product 1", 17.28);
+        SimpleProduct simpleProduct2 = new SimpleProduct("Product 2", 11.99);
+        SimpleProduct simpleProduct3 = new SimpleProduct("Product 3", 6.49);
 
         //When
-        invoice.addItem(new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0));
-        invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
-        invoice.addItem(new SimpleItem(new SimpleProduct("Product 3",  6.49), 5.0));
+        invoice.addItem(new SimpleItem(simpleProduct1, 2.0));
+        invoice.addItem(new SimpleItem(simpleProduct2, 3.5));
+        invoice.addItem(new SimpleItem(simpleProduct3, 5.0));
 
         //Then
         assertEquals(108.975, invoice.getValueToPay(), 0.001);
