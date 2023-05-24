a735423d0d152539d9e23465f837c338eb25525a
Norbert <norsol2200@gmail.com>
2023-05-24 13:50:08 +0200

    17.1 Joining Hibernate

---
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuElement.java
@@ -0,0 +1,12 @@
+package com.kodilla.sudoku;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SudokuElement {
+
+    private int element;
+    public static int EMPTY = -1;
+
+    List<Integer> elements = new ArrayList<>();
+}
